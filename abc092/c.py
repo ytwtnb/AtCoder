1d348d976ebcbcd46d65acadb4cbd6cc6faf055c
Yuta Watanabe <ytwtnb@gmail.com>
2018-04-07 14:07:22 +0900

    [abc092] update c

--- abc092/c.py
+++ abc092/c.py
@@ -1,13 +1,15 @@
 n = int(input())
 a = list(map(int, input().split()))
+s = abs(a[0])
+
+for i in range(n - 1):
+    s += abs(a[i] - a[i + 1])
+s += abs(a[n - 1])
 
 for i in range(n):
-    ans = 0
-    tmp = 0
-    for j in range(n):
-        if j != i:
-            ans += abs(tmp - a[j])
-            tmp = a[j]
-
-    ans += abs(tmp - 0)
-    print(ans)
+    if i == 0:
+        print(s + abs(0 - a[i + 1]) - (abs(0 - a[i]) + abs(a[i] - a[i + 1])))
+    elif i == n - 1:
+        print(s + abs(a[i - 1] - 0) - (abs(a[i - 1] - a[i]) + abs(a[i])))
+    else:
+        print(s + abs(a[i - 1] - a[i + 1]) - (abs(a[i - 1] - a[i]) + abs(a[i] - a[i + 1])))
